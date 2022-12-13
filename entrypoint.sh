aacfb916ffad356f7e01dcec817851030aabcc1f
Alessandro Delmastro <alessandro.delmas@di.unito.it>
2022-12-13 15:57:12 +0100

    Classi per la gestione del parametro sul verde, predisposto il modulo.

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-# $*  – bash Parameter Lists all the command line parameters in a single string format.
-exec java $JAVA_OPTS -jar otp.jar $*
--- src/main/java/org/opentripplanner/model/plan/AbsoluteDirection.java
+++
@@ -1,8 +0,0 @@
-package org.opentripplanner.model.plan;
-
-/** 
- * An absolute cardinal or intermediate direction.
- */
-public enum AbsoluteDirection {
-    NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST
-}

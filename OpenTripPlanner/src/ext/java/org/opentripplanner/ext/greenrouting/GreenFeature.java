56fa0e9c199f5ab8b6315a270a303bd34106acfa
Alessandro Delmastro <alessandro.delmas@di.unito.it>
2023-01-08 18:58:32 +0100

    Commenti.

--- OpenTripPlanner/src/ext/java/org/opentripplanner/ext/greenrouting/GreenFeature.java
+++ OpenTripPlanner/src/ext/java/org/opentripplanner/ext/greenrouting/GreenFeature.java
@@ -23,6 +23,12 @@ public class GreenFeature {
         this.geometry = geometry;
     }
 
+    /**
+     * Computes the kind of relation that exists among two line segments.
+     * @param ls1 the first line segment.
+     * @param ls2 the second line segment-
+     * @return a value that describes the relation.
+     */
     private Relation getRelation(LineSegment ls1, LineSegment ls2) {
         RobustLineIntersector rli = new RobustLineIntersector();
         rli.computeIntersection(
@@ -45,6 +51,13 @@ public class GreenFeature {
         return Relation.SEPARATED;
     }
 
+    /**
+     * Calculates the distance between the feature and another shape in terms
+     * of the relation between their geometries.
+     * @param g1 the geometry of the shape from which the distance is computed.
+     * @return a value indicating the minimum distance between this feature and
+     * the shape.
+     */
     public double getDistance(Geometry g1) {
         Geometry g2 = this.geometry;
         if (!(g1 instanceof LineString) || !(g2 instanceof LineString)) {
@@ -68,7 +81,11 @@ public class GreenFeature {
                 .get();
     }
 
-
+    /**
+     * Breaks a line into its segments.
+     * @param ls the line.
+     * @return a list of the segments that make up the line.
+     */
     private List<LineSegment> toLineSegments(LineString ls) {
         List<LineSegment> segments = new ArrayList<>();
         var points = ls.getCoordinates();
